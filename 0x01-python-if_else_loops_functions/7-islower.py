3f55af772cc9eb1ad07a598ad72482dfdf2d7117
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-01 14:27:22 -0500

    fixing style

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-	if ord(c) >= 65 and ord(c) <91:
-		return False
-	else:
-		return True
+    if ord(c) >= 65 and ord(c) < 91:
+        return False
+    else:
+        return True

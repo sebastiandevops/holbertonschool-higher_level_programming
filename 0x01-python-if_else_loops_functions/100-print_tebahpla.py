70ae2e6af32bf59b02a7282119ea5953687e3044
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-01 15:03:10 -0500

    fixing errors

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 != 0:
-        print("{}{}".format(chr(i), chr(i - 32)), end='')
+        print("{}{}".format(chr(i + 1), chr(i - 32)), end='')

6bed43726c25dcaa867b57f1c3843d0b42fbc54e
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-08 17:30:13 -0500

    fixing errors

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == ():
+    if len(sentence) == 0:
         result = (len(sentence), None)
     else:
         result = (len(sentence), sentence[0])

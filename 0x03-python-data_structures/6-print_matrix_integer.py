96e0da2bfd9b5c7f6d4788c80032585865c73078
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-08 17:18:53 -0500

    fixing errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
             if element != row[-1]:
-                print("{}".format(element), end=' ')
+                print("{:d}".format(element), end=' ')
             else:
-                print("{}".format(element), end='\n')
+                print("{:d}".format(element), end='\n')

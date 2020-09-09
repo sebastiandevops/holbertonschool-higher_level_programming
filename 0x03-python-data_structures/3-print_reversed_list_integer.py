991ce998163b7be734b9b633f4c339ac5f19be32
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-08 19:37:20 -0500

    fixing unknow error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for item in my_list:
-        print("{:d}".format(item))
+    if len(my_list) == 0:
+        print()
+    else:
+        my_list.reverse()
+        for item in my_list:
+            print("{:d}".format(item))

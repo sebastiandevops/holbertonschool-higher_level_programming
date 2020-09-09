4dcbb08fe148f2458e016d38b769ae1fbe5d9003
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-08 19:16:16 -0500

    fixing errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,9 +5,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif tuple_b == ():
         return tuple_a
     elif len(tuple_a) < 2:
-        new_tuple = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+        tuple_a = (*tuple_a, 0)
     elif len(tuple_b) < 2:
-        new_tuple = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-    else:
-        new_tuple = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+        tuple_b = (*tuple_b, 0)
+    new_tuple = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
     return new_tuple

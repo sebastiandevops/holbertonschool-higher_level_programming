fd302750016065f413f957f92152ae2e98dda472
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-10 22:07:36 -0500

    fixing

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary.values() == 0:
+    if a_dictionary is None or a_dictionary == 0:
         return None
     else:
         return max(a_dictionary.keys())

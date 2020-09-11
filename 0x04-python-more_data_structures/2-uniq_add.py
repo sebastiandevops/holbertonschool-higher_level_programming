ad5fdcab3d3b383ff984d25dcb761b8f3a5da4d9
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-10 21:59:46 -0500

    fixing errors

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniqList = []
-    if my_list == []:
-        print()
-    else:
-        for i in my_list:
-            if i not in uniqList:
-                uniqList.append(i)
-        return sum(uniqList)
+    for i in my_list:
+        if i not in uniqList:
+            uniqList.append(i)
+    return sum(uniqList)

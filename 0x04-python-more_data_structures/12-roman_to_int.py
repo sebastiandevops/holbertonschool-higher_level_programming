0aca515f2558631f7fe91d8e9340b197aa515307
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-10 22:14:42 -0500

    fixing errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    res = 0
-    i = 0
-    while (i < len(roman_string)):
-        s1 = value(roman_string[i])
-        if (i + 1 < len(roman_string)):
-            s2 = value(roman_string[i + 1])
-            if (s1 >= s2):
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    else:
+        res = 0
+        i = 0
+        while (i < len(roman_string)):
+            s1 = value(roman_string[i])
+            if (i + 1 < len(roman_string)):
+                s2 = value(roman_string[i + 1])
+                if (s1 >= s2):
+                    res = res + s1
+                    i = i + 1
+                else:
+                    res = res + s2 - s1
+                    i = i + 2
+            else:
                 res = res + s1
                 i = i + 1
-            else:
-                res = res + s2 - s1
-                i = i + 2
-        else:
-            res = res + s1
-            i = i + 1
-    return res
+        return res
 
 
 def value(r):

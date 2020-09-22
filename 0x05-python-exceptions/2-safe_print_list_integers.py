365d54d02401b44a015f0fdc6a335235d9b20ac0
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-21 19:15:53 -0500

    fixing errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,18 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     counter2 = 0
     newList = ""
     try:
-        for i in my_list:
-            counter2 += 1
-        for i in my_list[:x]:
-            if type(i) == int:
-                newList = newList + str(i)
+
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
                 counter1 += 1
-        num = int(newList)
-        if x > counter2:
-            print("{:d}".format(num), end='')
-            raise IndexError('list index out of range')
-        else:
-            print("{:d}".format(num))
+        print()
         return counter1
     except IndexError:
         raise

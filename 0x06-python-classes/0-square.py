a7f0038d24fe79dcab3f744b762c1dbe77f2a3fe
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-22 19:06:25 -0500

    task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
 class Square():
-    """empty class Square that defines a square
+    """empty class Square that defines a square.
+
+    Attributes:
+        Not Attributes for now.
+
     """

5c01a65d09702cd97efc150ad65c576dea91ecfb
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-22 19:18:03 -0500

    task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-class Square:
+class Square():
     """empty class Square that defines a square.
 
     Attributes:
         Not Attributes for now.
 
     """
+    def __init__(self):
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+           self
+
+        """

b341089c67041f80657e8d3b7e42ad552783b0c0
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-23 08:54:31 -0500

    fixing errrors after QA review

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter1 = 0
-    counter2 = 0
+    counter = 0
     try:
-        print(*my_list[:x], sep='')
-        for i in my_list:
-            counter1 += 1
-        return counter1
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        print()
+        return counter
     except IndexError:
-        for i in range(my_list[:counter1]):
-            print(*my_list[:counter1], sep='')
-            counter2 += 1
-        return counter2
+        raise

ce3d5821ce38561cdb68d9168d6c4e23a908564d
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-22 22:08:51 -0500

    5th task

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Class Square
+"""
+
+
+class Square:
+    """empty class Square that defines a square.
+
+    Attributes:
+        Not Attributes for now.
+
+    """
+    def __init__(self, size=0):
+        """Example of docstring on the __init__ method.
+
+        Args:
+            size (int): size of square.
+
+        """
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Class methods are similar to regular functions.
+
+        Returns:
+            Area of the square.
+
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Class methods are similar to regular functions.
+
+        Returns:
+            Size the square.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter size method.
+
+        Args:
+            value (int): new size of the square.
+
+        """
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #.add()
+
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end='')
+                print()

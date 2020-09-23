2387dc414f1593e44fa77fa6b4c526b9c6eb7713
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-23 08:55:07 -0500

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
         for i in range(x):
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             counter += 1
         print()
         return counter

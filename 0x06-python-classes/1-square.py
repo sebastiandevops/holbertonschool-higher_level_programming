05dfcbadb6338406a68bec8935bbc8d97969336f
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-22 19:47:36 -0500

    2nd task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class Square
+"""
+
+
+class Square:
+    """empty class Square that defines a square.
+
+    Attributes:
+        Not Attributes for now.
+
+    """
+    def __init__(self, size=None):
+        """Example of docstring on the __init__ method.
+
+        Args:
+            size (int): size of square.
+
+        """
+        self.__size = size

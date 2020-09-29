43586f393291a356ea44b051268bcd60aa99e514
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-29 18:33:00 -0500

    fixing errors

---
+++ 0x09-python-everything_is_object/101-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+LockedClass = __import__('101-LockedClass').LockedClass
+
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))

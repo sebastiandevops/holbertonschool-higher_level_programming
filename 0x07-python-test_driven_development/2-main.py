116d6a66db62ddb8f3bb00297f6bbfd9fcddb1ce
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-01 18:30:23 -0500

    fixing errors

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,7 +2,8 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [3]
+    [1, 2, 3],
+    [4, 5, 6]
 ]
-print(matrix_divided(matrix, 3))
+print(matrix_divided(matrix, div=float('inf')))
 print(matrix)

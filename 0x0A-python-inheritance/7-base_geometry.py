963ccdeeaec4a3da650050da89215269e16a3c80
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-06 21:19:19 -0500

    7th task

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry
+
+    Attributes:
+        Not Attributes for now.
+
+    """
+    def __init__(self):
+        """Init method.
+
+        Args:
+            None for now.
+
+        """
+        pass
+
+    def area(self):
+        """Area method.
+
+        Args:
+            None for now.
+
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method that validates value.
+
+        Args:
+            name (string): name of the object.
+            value (int): value to validate.
+
+        """
+        if type(value) != int:
+            raise TypeError(name + ' must be an integer')
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')

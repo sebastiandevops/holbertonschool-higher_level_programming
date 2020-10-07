45768ca6fefe6c3a44a8aa509b35707ffdedf3f9
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-06 19:18:28 -0500

    6th task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry
+
+    Attributes:
+        Not Attributes for now.
+
+    """
+    def __init__(self):
+        """Init method.
+
+        Args:
+            None for now.
+
+        """
+        pass
+
+    def area(self):
+        """Area method.
+
+        Args:
+            None for now.
+
+        """
+        raise Exception("area() is not implemented")

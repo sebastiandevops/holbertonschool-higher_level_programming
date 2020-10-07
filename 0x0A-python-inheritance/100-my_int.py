131588846b696375d0ded60292e8bf088031b6e1
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-06 23:57:20 -0500

    100th task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class MyInt
+"""
+
+
+class MyInt(int):
+    """class MyInt that inherits from List.
+
+    Attributes:
+        Not Attributes for now.
+
+    """
+    def __str__(self):
+        """str method.
+
+        Args:
+            None
+
+        Retuns:
+            Rectangle printed with the print_symbol.
+
+        """
+        if self:
+            return "%d" % self
+        if self == MyInt:
+            return "True"
+        else:
+            return "False"

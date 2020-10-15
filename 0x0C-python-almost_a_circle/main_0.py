79bfce876b7328c1d85a9b887c91c906ccbf8bb2
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-15 17:23:41 -0500

    fixing

--- 0x0C-python-almost_a_circle/main_0.py
+++ 0x0C-python-almost_a_circle/main_0.py
@@ -3,7 +3,7 @@
 from models.square import Square
 
 s = Square(12)
-print(s)
+print(s.size == 12)
 if s is None:
     print("Can't create Square")
     exit(1)
@@ -12,7 +12,7 @@ for attribute in list(s.__dict__.keys()):
     if "size" in attribute:
         print("You are not allowed to add any new attribute for size: {}".format(attribute))
         exit(1)
-
+print(s.size != 12)
 if s.size != 12:
     print("Wrong size getter: {}".format(s.size))
     exit(1)

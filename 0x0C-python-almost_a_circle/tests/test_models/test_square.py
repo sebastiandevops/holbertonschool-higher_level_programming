48d1b01fbc0a72b211c76efa75b9f500f711a6c7
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-15 22:48:01 -0500

    adding test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -201,5 +201,11 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(ValueError):
             Square(1, 2, -3)
 
+    def test_square_with_value_errors3(self):
+        """errors test
+        """
+        with self.assertRaises(ValueError):
+            Square(0)
+
 if __name__ == '__main__':
     unittest.main()

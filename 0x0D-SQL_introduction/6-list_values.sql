02877a71ebed7b245ecf91fb481a92dc9eb0752f
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-16 21:34:08 -0500

    8th task

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- script that creates a table called first_table in the current database in your MySQL server.
+-- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 SELECT * FROM first_table;

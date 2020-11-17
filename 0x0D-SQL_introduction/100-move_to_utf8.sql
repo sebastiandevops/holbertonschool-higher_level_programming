dd8cd5138f88915153493132a72650105be64c68
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-16 23:44:05 -0500

    100 task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,4 @@
 -- in your MySQL server.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb COLLATE utf8mb4_unicode_ci;

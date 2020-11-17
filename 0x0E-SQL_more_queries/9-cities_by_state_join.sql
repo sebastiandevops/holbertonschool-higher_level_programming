5af9fb32222088f40aa1c731dc5198e9bc394fe8
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-17 17:56:22 -0500

    10th task

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT cities.id, cities.name, states.name
     FROM cities
-    RIGHT JOIN states
-    ON cities.id = states.id
+    LEFT JOIN states
+    ON cities.state_id = states.id
     ORDER BY cities.id ASC;

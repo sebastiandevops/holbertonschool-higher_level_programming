c99d0f512fb0955ca44c1da187b61a4af9dce003
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-16 22:11:20 -0500

    11th task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
--- script that creates a table called first_table in the current database in your MySQL server.
+-- script that creates a table second_table in the database hbtn_0c_0 in your
+-- MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);

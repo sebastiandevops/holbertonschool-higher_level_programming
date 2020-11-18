57197bb310772734bb7b8287bc060a3d8b48e9ff
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-17 23:48:31 -0500

    14th task

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,7 +8,7 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name AS genre,
-    COUNT(tv_show_genres.genre_id) as number_of_shows
+    COUNT(tv_show_genres.genre_id) AS number_of_shows
     FROM tv_genres
     LEFT JOIN tv_show_genres
     ON tv_show_genres.genre_id = tv_genres.id

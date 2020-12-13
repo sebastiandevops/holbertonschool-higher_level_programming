2a89e1dfb2784eb5fe0f8412631da064958ab8e4
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-13 00:13:34 -0500

    3rd task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa where name matches the argument.
+The script take 3 arguments: mysql username, mysql password and
+database name and state name searched.
+The script connects to MySQL server running on localhost at port 3306.
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+The code should not be executed when imported.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    var = sys.argv[4]
+    c.execute("SELECT * FROM states\
+               WHERE name = %s\
+               ORDER BY id ASC", (var,))
+    tables = c.fetchall()
+    for items in tables:
+        print(items)
+    c.close()
+    db.close()

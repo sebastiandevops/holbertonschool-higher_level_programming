a4ecbad4739e48dbec080c2f92faad32e37dccc5
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-12 22:32:41 -0500

    correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("USE hbtn_0e_0_usa")

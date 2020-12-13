9198ac0a417b136aa7d9eaefc555775a0b60df8a
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-12 22:58:46 -0500

    second task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+The script take 3 arguments: mysql username, mysql password and
+database name (no argument validation needed)
+The script connects to MySQL server running on localhost at port 3306.
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+The code should not be executed when imported.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    tables = c.fetchall()
+    for items in tables:
+        print(items)
+    c.close()
+    db.close()

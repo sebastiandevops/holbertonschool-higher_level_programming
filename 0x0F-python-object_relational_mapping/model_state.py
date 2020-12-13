7fa232c02296b685b96c54c4b417a3f81ee2c3da
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-13 16:26:29 -0500

    7th task

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class BaseGeometry
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class:
+    inherits from Base Tips
+    links to the MySQL table states
+
+    class attributes:
+        id: that represents a column of an auto
+        generated, unique integer, can’t be null and is a primary key
+        name: that represents a column of a string with maximum 128
+        characters and can’t be null.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)

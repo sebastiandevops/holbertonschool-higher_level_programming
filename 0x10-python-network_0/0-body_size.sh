2f33f25f92144cac5ab908bc84f998a6ac26030c
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-18 18:06:31 -0500

    first task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-URL="http://daf2007ee0ed.e0e9d352.hbtn-cod.io:5000"
-curl -sI $URL | grep -i Content-Length | awk '{print $2}'
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "http://daf2007ee0ed.e0e9d352.hbtn-cod.io:5000" | grep -i Content-Length | awk '{print $2}'

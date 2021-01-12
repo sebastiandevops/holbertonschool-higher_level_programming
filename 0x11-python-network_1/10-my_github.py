7d3d0e22ca631c76f020393f10c0ab738fc8b19e
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-11 23:02:39 -0500

    10th task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,10 @@ import sys
 
 if __name__ == "__main__":
     user = sys.argv[1]
-    url = 'https://api.github.com/' + user
+    url = 'https://api.github.com/user'
     pw = sys.argv[2]
     r = requests.get(url, auth=HTTPBasicAuth(user, pw))
-    print(r.json()['id'])
+    try:
+        print(r.json()['id'])
+    except KeyError:
+        print("None")

4f6670ec6929c2f955a6a7bfc560730fc9984c70
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-02-02 16:13:07 -0500

    7 task correction

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ if (!Number.isInteger(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
   while (i < parseInt(process.argv[2])) {
-    console.log('c is fun');
+    console.log('C is fun');
     i++;
   }
 }

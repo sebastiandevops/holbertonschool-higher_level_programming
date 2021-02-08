77b785309d174c44e8549081034f6399329af032
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-02-08 17:12:32 -0500

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+'use strict';
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let stri = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        stri += 'X';
+      }
+      if (i < (this.height - 1)) {
+        stri += '\n';
+      }
+    }
+    console.log(stri);
+  }
+};

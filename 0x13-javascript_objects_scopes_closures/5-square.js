6a5a90a80b623704f644ede7e62476c3c7fe3ec5
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-02-08 18:32:55 -0500

    correction

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,9 +4,6 @@ const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.size = size;
-    super.width = size;
-    super.height = size;
+    super(size, size);
   }
 };
